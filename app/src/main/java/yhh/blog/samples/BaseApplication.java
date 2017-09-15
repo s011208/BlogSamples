b288e3a93c3cfddd98c981d2310bca0a827bca72
s011208 <s011208@gmail.com>
2017-09-15 14:32:54 +0800

    get context from BaseApplication

--- app/src/main/java/yhh/blog/samples/BaseApplication.java
+++ app/src/main/java/yhh/blog/samples/BaseApplication.java
@@ -1,7 +1,10 @@
 package yhh.blog.samples;
 
 import android.app.Application;
+import android.content.Context;
 
 public interface BaseApplication {
     void onCreate(Application application);
+
+    Context getContext();
 }
--- app/src/main/java/yhh/blog/samples/mvp/MVPApplication.java
+++ app/src/main/java/yhh/blog/samples/mvp/MVPApplication.java
@@ -1,13 +1,21 @@
 package yhh.blog.samples.mvp;
 
 import android.app.Application;
+import android.content.Context;
 
 import yhh.blog.samples.BaseApplication;
 
 public class MVPApplication implements BaseApplication {
+    private Application mApplication;
+
     @Override
     public void onCreate(Application application) {
+        mApplication = application;
+    }
 
+    @Override
+    public Context getContext() {
+        return mApplication;
     }
 
 }
