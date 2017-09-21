4da332b0e44082f072319fd9f62f30ed2b83d2cc
s011208 <s011208@gmail.com>
2017-09-22 03:52:20 +0800

    add a dagger2 blog link

---
+++ app/src/main/java/yhh/blog/samples/Links.java
@@ -0,0 +1,9 @@
+package yhh.blog.samples;
+
+public class Links {
+    /**
+     * dagger2:
+     * https://blog.mindorks.com/introduction-to-dagger-2-using-dependency-injection-in-android-part-2-b55857911bcd
+     *
+     */
+}
